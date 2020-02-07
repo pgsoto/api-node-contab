8c25dc410a747acc6d7a238dfe77add8713e90be
Pedro Soto Soto <dex.pgsoto@gmail.com>
2020-02-07 06:56:25 -0300

    ajuste port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./src/app')
 const PORT = process.env.APP_SERVER_PORT || 3000;
 
 async function main(){
-    await app.listen(PORT)
+    await app.listen(PORT || 8000)
     console.log('Server funciona bien! en puerto: ', PORT)
 }
 
