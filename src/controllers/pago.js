292f9e15fcac820f14684d109fa7726a0a8921c6
Pedro Soto Soto <dex.pgsoto@gmail.com>
2020-02-08 22:03:37 -0300

    solucion validacion pago existente

--- src/controllers/pago.js
+++ src/controllers/pago.js
@@ -37,33 +37,32 @@ ctrl.insert = async(req, res) => {
 
     await Pago.findOne({cliente:body.cliente, tipo:body.tipo, fecha:body.fecha}, (err, result) => {
         if (err) {
-            return res.status(500).json({
+            res.status(500).json({
                 ok: false,
                 err
             })
         }
 
         if (result) {
-            return res.status(400).json({
-                ok: false,
-                error: "Pago ya existe"
+            res.status(400).json({
+                status: 'error',
+                message: 'Pago ya existe',
+              })
+        } else{
+            pago.save((err, pagoDB) => {
+                if (err) {
+                    res.status(500).json({
+                        ok: false,
+                        err
+                    })
+                }
+        
+                res.status(201).json({
+                    ok: true,
+                    pago: pagoDB
+                })
             })
         }
-    })
-
-    pago.save((err, pagoDB) => {
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                err
-            })
-        }
-
-        res.status(201).json({
-            ok: true,
-            pago: pagoDB
-        })
-
     })
 }
 
